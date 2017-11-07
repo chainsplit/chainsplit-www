71ab3065ea81642e5b80c35de62d8d6d854927a8
Taylor Brink <taylor@yahoo.com>
2017-11-07 06:55:17 -0600

    new gh-pages build

---
+++ assets/js/lang.js
@@ -0,0 +1,34 @@
+$(function() {
+
+  var baseUrl = '/chainsplit-www';
+  var path = window.location.pathname;
+
+  // lang-select selection based on language from url path
+  if( path.match(/\/[a-z]{2}\//) ) {
+    var lang = path.match(/\/[a-z]{2}\//)[0].replace(/\//g, '');
+    $('.lang-select select').val(lang);
+  }
+
+  // change url on language selection
+  $('.lang-select select').on('change', function() {
+    // support baseurl (preview mode)
+    if( path.includes(baseUrl + '/') ) {
+      path = path.replace(baseUrl, '');
+    } else {
+      baseUrl = '';
+    }
+
+    // remove old lang path
+    path = path.replace(/\/[a-z]{2}\//, '');
+
+    // build new path
+    if( this.value == 'en' ) {
+      path = baseUrl + path;
+    } else {
+      path = baseUrl + '/' + this.value + path;
+    }
+
+    window.location = window.location.origin + path;
+  });
+
+});
